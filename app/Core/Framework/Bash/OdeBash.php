2d1c77846b6a5a6cdce26b938ce2201150db948c
Haniel <contact@cendre.org>
2016-01-23 19:26:54 +0100

    Adding a console

---
+++ app/Core/Framework/Bash/OdeBash.php
@@ -0,0 +1,15 @@
+<?php
+namespace Framework\Bash;
+
+class OdeBash extends CommandManager {
+
+    /**
+     * @desc
+     * @return string
+     */
+    public function pute()
+    {
+        return "OUI J'SUIS UNE GROSSE PUTE";
+    }
+
+}
---
+++ console.php
@@ -0,0 +1,10 @@
+<?php
+require_once "./app/init.php";
+require_once "./vendor/autoload.php";
+require_once "./app/autoloader.php";
+
+
+$logger = new \Framework\Logger('logs/console', \Framework\Logger::LOG_LEVEL_DEBUG);
+$bash = new \Framework\Bash\BashController($logger);
+
+$bash->init();
